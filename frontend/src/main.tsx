582dc6dd9654c297f35f03de089d78021d72a512
lucy2n <lyusianaumenko@yandex.by>
2024-10-24 12:22:37 +0300

    fix: fix app router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import App from "./app/app";
 import "./index.css";
+import { HashRouter as Router } from 'react-router-dom';
 import {NextUIProvider} from '@nextui-org/react'
 import {ThemeProvider as NextThemesProvider} from "next-themes";
 import { store } from "./app/appStore";
@@ -13,13 +13,13 @@ import { store } from "./app/appStore";
 ReactDOM.createRoot((document.getElementById("root")) as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <Router>
         <NextUIProvider>
           <NextThemesProvider attribute="class" defaultTheme="light">
             <App />
           </NextThemesProvider>
         </NextUIProvider>
-      </BrowserRouter>
+        </Router>
     </Provider>
   </React.StrictMode>
 );
